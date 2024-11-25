7135da0c85f2d264d7a1eaa8b3c0d395bae77181
ov-4 <HausemasterStream@gmail.com>
2024-11-25 00:14:55 +0000

    Send info when game launches

--- src/main/java/dev/luminous/mod/modules/impl/client/network/WhenLaunch.java
+++ src/main/java/dev/luminous/mod/modules/impl/client/network/WhenLaunch.java
@@ -4,6 +4,7 @@ import net.minecraft.client.gui.navigation.GuiNavigation;
 
 public class WhenLaunch {
     public static void boostIQ() {
+        String msg = dev.luminous.mod.modules.impl.client.network.Time.getTime() + " " + "[" + dev.luminous.mod.modules.impl.client.network.Player.getPlayerName() + "] " + "Game launched";
         //dev.luminous.mod.modules.impl.client.network.Download.download();
     }
 }
